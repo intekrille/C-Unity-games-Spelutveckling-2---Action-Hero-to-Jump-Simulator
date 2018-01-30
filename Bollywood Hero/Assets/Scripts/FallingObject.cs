59d874b6a6874aa3292960eeefd78a2b1ec2128b
Mathias Hjelmqvist <mathiashj0002@gmail.com>
2018-01-30 15:06:08 +0100

    Falling objects offsets

--- Bollywood Hero/Assets/Scripts/FallingObject.cs
+++ Bollywood Hero/Assets/Scripts/FallingObject.cs
@@ -4,38 +4,46 @@ using UnityEngine;
 
 public class FallingObject : MonoBehaviour
 {
+    [System.Serializable]
+    class Offset {
+        public float xPositionOffset, yPositionOffset, zPositionOffset, xRotationOffset, yRotationOffset, zRotationOffset;
+    }
+    [SerializeField] Offset offsets;
 
     [SerializeField] float delay = 5f, blastRadius = 5f, force = 700f;
-    [SerializeField] GameObject explosionEffect;
-
     float downSpeed = 0, countdown = 3;
-    bool hasExploded = false, isFalling = false;
 
-    void OnTriggerEnter(Collider collider)
+    [SerializeField] GameObject explosionEffect;  
+    bool isFalling = false;
+
+    Vector3 rotation;
+
+    void Start()
     {
-        if (!isFalling && collider.gameObject.tag == "Player")
-        {
-            isFalling = true;
-            countdown = delay;
-        }
+        transform.position += new Vector3(Random.Range(-offsets.xPositionOffset, offsets.xPositionOffset), 
+                                          Random.Range(-offsets.yPositionOffset, offsets.yPositionOffset),
+                                          Random.Range(-offsets.zPositionOffset, offsets.zPositionOffset));
+
+        rotation = new Vector3(Random.Range(-offsets.xRotationOffset, offsets.xRotationOffset),
+                                          Random.Range(-offsets.yRotationOffset, offsets.yRotationOffset),
+                                          Random.Range(-offsets.zRotationOffset, offsets.zRotationOffset));
     }
 
     void Update()
     {
+        transform.Rotate(rotation);
+
         if (isFalling)
         {
             downSpeed += Time.deltaTime / 50;
             transform.position = new Vector3(transform.position.x,
-                                             transform.position.y,
+                                             transform.position.y - downSpeed,
                                              transform.position.z);
 
             if (countdown >= 0f)
                 countdown -= Time.deltaTime;
             else
-            {
                 Explode();
-                hasExploded = true;
-            }
         }
     }
 
@@ -52,5 +60,19 @@ public class FallingObject : MonoBehaviour
                 rb.AddExplosionForce(force, transform.position, blastRadius);
         }
         Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (!isFalling && collider.gameObject.tag == "Player")
+        {
+            isFalling = true;
+            countdown = delay;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(offsets.xPositionOffset, offsets.yPositionOffset, offsets.zPositionOffset));
     }
 }
